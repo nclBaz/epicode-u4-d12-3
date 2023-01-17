2069e5fc63772cc68133127dc9efab7af4092c45
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-01-17 10:49:36 +0100

    Adds POST & GET /users

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,5 +1,48 @@
 import express from "express"
+import UsersModel from "./model.js"
 
 const usersRouter = express.Router()
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new UsersModel(req.body)
+    // here it happens validation (thanks to Mongoose) of req.body, if it is not ok Mongoose will throw an error
+    // if it is ok the user is not saved yet
+    const { _id } = await newUser.save()
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await UsersModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
 
 export default usersRouter
